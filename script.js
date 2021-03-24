1bfa48f356418295db0e70e098d62e63f8dc5145
Alex <cigan@ua.fm>
2021-03-24 16:33:22 +0200

    feat: add functionality to the button Reset

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const fullscreenButton = document.querySelector('.fullscreen');
 
+const btnContainer = document.querySelectorAll('.btn-container');
+
+const inputsContainer = document.querySelectorAll('.filters');
+
+const btnReset = document.querySelector('.btn-reset');
+
+
 fullscreenButton.addEventListener('click', () => {
   !document.fullscreenElement ? document.documentElement.requestFullscreen() : document.exitFullscreen();
 });
-
-const btnContainer = document.querySelectorAll('.btn-container');
 
 const addActiveClass = (e) => e.target.classList.add('btn-active');
 const removeActiveClass = (btn) => {
@@ -17,18 +22,21 @@ btnContainer.forEach(btn => btn.addEventListener('click', (e) => {
   addActiveClass(e);
 }));
 
-
-const inputsContainer = document.querySelectorAll('.filters');
-
-inputsContainer.forEach(el => {
-  inputsContainer.forEach(inputEl => {
-    inputEl.addEventListener('change', (e) => {
-      document.documentElement.style.setProperty(`--${e.target.name}`, `${e.target.value}` + `${e.target.dataset.sizing}`);
-      e.target.nextElementSibling.value = e.target.value
-      // console.log(e.target.nextElementSibling.value)
-
-    })
+inputsContainer.forEach(inputEl => {
+  inputEl.addEventListener('change', (e) => {
+    document.documentElement.style.setProperty(`--${e.target.name}`, `${e.target.value}` + `${e.target.dataset.sizing}`);
+    console.log(`--${e.target.name}`, `${e.target.value}` + `${e.target.dataset.sizing}`)
+    e.target.nextElementSibling.value = e.target.value
   })
 })
 
-// console.log(inputsContainer.getAttributeNames('blur'))
+btnReset.addEventListener('click', () => {
+  inputsContainer.forEach(el => {
+    const inputs = el.querySelectorAll('input');
+    inputs.forEach(input => {
+      document.documentElement.style.setProperty(`--${input.name}`, `${input.dataset.value}` + `${input.dataset.sizing}`);
+      input.nextElementSibling.value = input.dataset.value;
+      input.value = input.dataset.value;
+    });
+  })
+});
