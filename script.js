54644b03a15511455ec663801f36ee52acc3b19a
Alex <cigan@ua.fm>
2021-03-24 20:24:01 +0200

    add functionality to the Next picture button

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const btnContainer = document.querySelectorAll('.btn-container');
 const inputsContainer = document.querySelectorAll('.filters');
 
 const btnReset = document.querySelector('.btn-reset');
+const btnNext = document.querySelector('.btn-next');
 
 
 fullscreenButton.addEventListener('click', () => {
@@ -27,7 +28,6 @@ btnContainer.forEach(btn => btn.addEventListener('click', (e) => {
 inputsContainer.forEach(inputEl => {
   inputEl.addEventListener('input', (e) => {
     document.documentElement.style.setProperty(`--${e.target.name}`, `${e.target.value}` + `${e.target.dataset.sizing}`);
-    console.log(`--${e.target.name}`, `${e.target.value}` + `${e.target.dataset.sizing}`)
     e.target.nextElementSibling.value = e.target.value
   })
 })
@@ -42,3 +42,47 @@ btnReset.addEventListener('click', () => {
     });
   })
 });
+
+const picture = document.getElementById('image');
+const pictureSrc = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/'
+const currentHour = new Date().getHours();
+const imagesNumbers = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+let counter = 0;
+
+const getTimeOfDay = (hour = currentHour) => {
+  let timeOfDay = '';
+  if (hour >= 6 && hour < 12) {
+    timeOfDay = 'morning';
+  }
+  if (hour >= 12 && hour < 18) {
+    timeOfDay = 'day';
+  }
+  if (hour >= 18 && hour < 24) {
+    timeOfDay = 'evening';
+  }
+  if (hour >= 0 && hour < 6) {
+    timeOfDay = 'night';
+  }
+  return timeOfDay;
+}
+
+// const viewImage = (src) => {
+//   const img = new Image();
+//   img.src = src;
+//   img.onload = () => {
+//     body.style.backgroundImage = `url(${src})`;
+//   };
+// }
+
+const getImage = (e) => {
+  const index = counter % imagesNumbers.length;
+  picture.src = pictureSrc + getTimeOfDay() + '/' + imagesNumbers[index];
+  counter++;
+}
+
+
+btnNext.addEventListener('click', (e) => {
+
+  getImage(e)
+
+})
